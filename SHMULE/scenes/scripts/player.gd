dd9c99af108629010e24070f6b2b0e727467c099
Samuel Hoekwater <shoek@umich.edu>
2024-04-18 23:27:40 -0500

    Added background and double jump

--- SHMULE/scenes/scripts/player.gd
+++ SHMULE/scenes/scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+# jump count
+var can_double_jump = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,16 +17,22 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			can_double_jump = true
+			velocity.y = JUMP_VELOCITY
+			anim.play("Jump")
+		elif can_double_jump:
+			can_double_jump = false
+			velocity.y = JUMP_VELOCITY
+			anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
-	else:
+	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 	if direction:
 		velocity.x = direction * SPEED
