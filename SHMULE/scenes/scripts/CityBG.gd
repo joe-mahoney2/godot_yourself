459dadb9238ae7bf3f616fe6dd81c283dd7dc441
Samuel Hoekwater <shoek@umich.edu>
2024-07-12 22:34:56 -0500

    Made a template animation and put him in to try and make a player with him.

--- SHMULE/scenes/scripts/CityBG.gd
+++ SHMULE/scenes/scripts/CityBG.gd
@@ -2,6 +2,6 @@ extends ParallaxBackground
 
 
 var scrolling_speed = 100
-
-func _process(delta):
-	scroll_offset.x -= scrolling_speed * delta
+#
+#func _process(delta):
+	#scroll_offset.x -= scrolling_speed * delta
--- SHMULE/scenes/scripts/Frog.gd
+++ SHMULE/scenes/scripts/Frog.gd
@@ -6,11 +6,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 func _physics_process(delta):
 	# Gravity for the froggy
 		
 	velocity.y += gravity * delta
 	if chase == true:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
@@ -18,12 +23,28 @@ func _physics_process(delta):
 		elif direction.x < 0:
 			get_node("AnimatedSprite2D").flip_h = false
 		velocity.x = direction.x * SPEED
+	else:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
 	move_and_slide()
 
+# Froggy Field of view
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		chase = true;
 
 func _on_player_detection_body_exited(body):
-	if body.name != "Player":
+	if body.name == "Player":
 		chase = false;
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		chase == false
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
+
+func _on_player_death_body_exited(body):
+	pass # Replace with function body.
